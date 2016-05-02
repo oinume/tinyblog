deaf39024173a3d526ddddfd97695a9fd60634b3
Kazuhiro Oinuma <oinume@gmail.com>
2016-05-02 22:55:42 +0900

    Add task generate_data

--- task/db.py
+++ task/db.py
@@ -1,7 +1,7 @@
 # encoding: utf-8
 from __future__ import print_function
 from fabric.api import abort, env, lcd, local, task
-
+import pymysql
 
 @task
 def migrate(version):
@@ -17,3 +17,19 @@ def reset():
 @task
 def mysqldump():
     local("""vagrant ssh -- "cd /vagrant && mysqldump -uroot -proot --single-transaction --order-by-primary --compress --compact --add-drop-database --default-character-set=utf8mb4 tinyblog > tinyblog.dump.sql" """)
+
+
+@task
+def generate_data():
+    conn = pymysql.connect(
+        host="192.168.9.10",
+        db="tinyblog",
+        user="tinyblog",
+        password="tinyblog",
+        charset="utf8mb4",
+        cursorclass=pymysql.cursors.DictCursor)
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT 1")
+        for row in cursor.fetchall():
+            print(row)
+    conn.close()
