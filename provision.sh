d3678c0b9782619e1fd82611e2e0a56e609fea68
Kazuhiro Oinuma <oinume@gmail.com>
2016-05-02 15:40:04 +0900

    Modify provision

--- provision.sh
+++ provision.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-sudo apt install mysql-server-5.6 mysql-client-5.6
-sudo apt install python-pip python-dev
+sudo apt update
+echo "mysql-server-5.6  mysql-server/root_password password root" | sudo debconf-set-selections
+echo "mysql-server-5.6  mysql-server/root_password_again password root" | sudo debconf-set-selections
+sudo apt install -y mysql-server-5.6 mysql-client-5.6 libmysqlclient-dev
+mysql -uroot -proot -e "CREATE DATABASE IF NOT EXISTS tinyblog DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_general_ci;"
+mysql -uroot -proot -e "GRANT ALL ON tinyblog.* TO tinyblog@'%' IDENTIFIED BY 'tinyblog';"
+mysql -uroot -proot -e "GRANT ALL ON tinyblog.* TO tinyblog@'localhost' IDENTIFIED BY 'tinyblog';"
+
+sudo apt install -y python-pip python-dev
 sudo pip install -r requirements.txt
