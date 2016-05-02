ad93a106654217e9290ebc11100f1c31ab01034e
Kazuhiro Oinuma <oinume@gmail.com>
2016-05-02 20:59:34 +0900

    Add foreign key constraint

--- fabfile.py
+++ fabfile.py
@@ -9,5 +9,5 @@ def migrate():
 
 @task
 def reset_db():
-    local("""vagrant ssh -- "mysql -uroot -e \'DROP DATABASE tinyblog\'" """)
-    local("""vagrant ssh -- "mysql -uroot -e \'CREATE DATABASE tinyblog DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci\'" """)
+    local("""vagrant ssh -- "mysql -uroot -proot -e \'DROP DATABASE tinyblog\'" """)
+    local("""vagrant ssh -- "mysql -uroot -proot -e \'CREATE DATABASE tinyblog DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci\'" """)
